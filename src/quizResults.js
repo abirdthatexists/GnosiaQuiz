13032dd2a9aef7f320e1790ab74c295be05ba718
Peregrine <abirdthatexists@gmail.com>
2025-11-29 00:24:20 -0500

    initial commit - made chart

---
+++ src/quizResults.js
@@ -0,0 +1,213 @@
+const data = {
+  labels: [
+    'Charisma',
+    'Intuition',
+    'Logic',
+    'Charm',
+    'Stealth',
+    'Performance'
+  ],
+  datasets: [{
+      label: 'Results',
+    data: [25, 25, 25, 25, 25, 25],
+    fill: true,
+    backgroundColor: 'rgba(90, 101, 117, 0.2)',
+    borderColor: 'rgb(90, 101, 117)',
+    pointBackgroundColor: 'rgb(90, 101, 117)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(90, 101, 117)'
+  },{
+    label: 'Setsu',
+    data: [35, 28.5, 38.5, 36.5, 31, 17.5],
+    fill: true,
+    backgroundColor: 'rgba(49, 159, 154, 0.2)',
+    borderColor: 'rgb(49, 159, 154)',
+    pointBackgroundColor: 'rgb(49, 159, 154)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(49, 159, 154)',
+    hidden: true
+  }, {
+    label: 'Gina',
+    data: [17.5, 45.5, 31.5, 24, 13, 31.5],
+    fill: true,
+    backgroundColor: 'rgba(128, 51, 146, 0.2)',
+    borderColor: 'rgb(128, 51, 146)',
+    pointBackgroundColor: 'rgb(128, 51, 146)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(128, 51, 146)',
+    hidden: true
+  },{
+    label: 'SQ',
+    data: [22, 21.5, 12, 46, 47.5, 38.5],
+    fill: true,
+    backgroundColor: 'rgba(229, 0, 58, 0.2)',
+    borderColor: 'rgba(229, 0, 58)',
+    pointBackgroundColor: 'rgba(229, 0, 58)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(229, 0, 58)',
+    hidden: true
+  },{
+    label: 'Raqio',
+    data: [22, 21.5, 12, 46, 47.5, 38.5],
+    fill: true,
+    backgroundColor: 'rgba(229, 0, 58, 0.2)',
+    borderColor: 'rgba(229, 0, 58)',
+    pointBackgroundColor: 'rgba(229, 0, 58)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(229, 0, 58)',
+    hidden: true
+  },{
+    label: 'Stella',
+    data: [27, 18, 42, 27.5, 30.5, 29],
+    fill: true,
+    backgroundColor: 'rgba(2, 167, 77,0.2)',
+    borderColor: 'rgb(2, 167, 77)',
+    pointBackgroundColor: 'rgb(2, 167, 77)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(2, 167, 77)',
+    hidden: true
+  },{
+    label: 'Shigemichi',
+    data: [45.5, 14.5, 9.5, 17.5, 6, 45],
+    fill: true,
+    backgroundColor: 'rgba(170, 207, 83, 0.2)',
+    borderColor: 'rgba(170, 207, 83)',
+    pointBackgroundColor: 'rgba(170, 207, 83)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(170, 207, 83)',
+    hidden: true
+  },{
+    label: 'Chipie',
+    data: [25, 39, 18.5, 31, 16.5, 33.5],
+    fill: true,
+    backgroundColor: 'rgba(244, 152, 28, 0.2)',
+    borderColor: 'rgba(244, 152, 28)',
+    pointBackgroundColor: 'rgba(244, 152, 28)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(244, 152, 28)',
+    hidden: true
+  },{
+    label: 'Comet',
+    data: [17, 49.5, 0.5, 32.5, 16.5, 22.5],
+    fill: true,
+    backgroundColor: 'rgba(255, 244, 6, 0.2)',
+    borderColor: 'rgba(255, 244, 6)',
+    pointBackgroundColor: 'rgba(255, 244, 6)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(255, 244, 6)',
+    hidden: true
+  },{
+    label: 'Jonas',
+    data: [38.5, 25, 34, 21.5, 43.5, 37],
+    fill: true,
+    backgroundColor: 'rgba(125, 93, 83, 0.2)',
+    borderColor: 'rgba(125, 93, 83)',
+    pointBackgroundColor: 'rgba(125, 93, 83)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(125, 93, 83)',
+    hidden: true
+  },{
+    label: 'Kukrushka',
+    data: [14, 35.5, 3.5, 49.5, 45, 40.5],
+    fill: true,
+    backgroundColor: 'rgba(129, 0, 7, 0.2)',
+    borderColor: 'rgba(129, 0, 7)',
+    pointBackgroundColor: 'rgba(129, 0, 7)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(129, 0, 7)',
+    hidden: true
+  },{
+    label: 'Otome',
+    data: [16, 32, 46.5, 42, 23, 26.5],
+    fill: true,
+    backgroundColor: 'rgba(250, 220, 235, 0.2)',
+    borderColor: 'rgba(250, 220, 235)',
+    pointBackgroundColor: 'rgba(250, 220, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(250, 220, 235)',
+    hidden: true
+  },{
+    label: 'Sha-Ming',
+    data: [29, 6.5, 6.5, 34.5, 40.5, 49.5],
+    fill: true,
+    backgroundColor: 'rgba(35, 23, 23, 0.2)',
+    borderColor: 'rgba(35, 23, 23)',
+    pointBackgroundColor: 'rgba(35, 23, 23)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(35, 23, 23)',
+    hidden: true
+  },{
+    label: 'Remnan',
+    data: [2, 41, 28, 29, 33, 43.5],
+    fill: true,
+    backgroundColor: 'rgba(254, 254, 252, 0.2)',
+    borderColor: 'rgba(254, 254, 252)',
+    pointBackgroundColor: 'rgba(254, 254, 252)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(254, 254, 252)',
+    hidden: true
+  },{
+    label: 'Yuriko',
+    data: [49.5, 42, 44, 37.5, 49.5, 25],
+    fill: true,
+    backgroundColor: 'rgba(156, 157, 150, 0.2)',
+    borderColor: 'rgba(156, 157, 150)',
+    pointBackgroundColor: 'rgba(156, 157, 150)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(156, 157, 150)',
+    hidden: true
+  }]
+};
+var options = {
+    scale: {
+        r: {
+            min: 0,
+            max: 50,
+
+        }
+    }
+};
+
+
+const ctx = document.getElementById('quizResults');
+
+radarChart = new Chart(ctx, {
+    type: 'radar',
+    data: data,
+    options: options
+});
+
+function toggle(id) {
+    const data = radarChart.data.datasets[id];
+    data.hidden = !data.hidden;
+    radarChart.update();
+}
+
+function showAll() {
+    for (let i = 0; i <radarChart.data.datasets.length; i++) {
+        radarChart.data.datasets[i].hidden = false;
+    }
+    radarChart.update();
+}
+
+function hideAll() {
+    for (let i = 0; i <radarChart.data.datasets.length; i++) {
+        radarChart.data.datasets[i].hidden = true;
+    }
+    radarChart.update();
+}
